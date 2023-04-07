7783421dab677909a9e5b27cb771d8730820d36e
Faraz <farazatgithub@gmail.com>
2023-04-07 15:58:19 +0500

    pagination using skip and limit and page

--- controllers/products.js
+++ controllers/products.js
@@ -12,7 +12,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   // req.query to access the query params passed to the request
-  const { name, company, price, sort, fields, limit, skip } = req.query;
+  const { name, company, price, sort, fields } = req.query;
   const queryObj = {};
 
   if (name) {
@@ -40,14 +40,18 @@ const getAllProducts = async (req, res) => {
 
   //   throw new Error("oops")
 
-  let products;
   if (fields) {
     const fieldList = fields.split(",").join(" ");
-    products = await result.select(fieldList).limit(limit).skip(skip);
-  } else {
-    products = await result.limit(limit).skip(skip);
-  }
+    result =  result.select(fieldList);
+  } 
 
+   const page = Number(req.query.page) || 1
+   const limit = Number(req.query.limit) ||  10
+   const skip = (page - 1) * limit 
+
+   result = result.skip(skip).limit(limit)
+
+  const products = await result 
   return res.status(200).json({ products });
 };
 
